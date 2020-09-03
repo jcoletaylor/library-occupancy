1e99d023ca4d77dfe176be4bc186af7608faf834
Pete Taylor <j.cole.taylor@gmail.com>
2020-09-03 19:11:10 -0400

    serving react natively now

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
+const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const { publicFilePath } = require('./config/settings')
-
+const staticFiles = path.join(publicFilePath, 'static')
+const reactFiles = path.join(publicFilePath, 'react')
 const { routes } = require('./routes')
 
 const app = express()
@@ -11,7 +13,11 @@ app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use('/', routes)
-app.use('/static', express.static(publicFilePath))
+app.use('/static', express.static(staticFiles))
+app.use(express.static(reactFiles))
+app.get('/react', function (req, res) {
+  res.sendFile(path.join(reactFiles, 'index.html'))
+})
 
 module.exports = {
   app
