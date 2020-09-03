e866092131f099e28e8d4860854588250d0dafba
Pete Taylor <j.cole.taylor@gmail.com>
2020-09-03 18:46:01 -0400

    moving files around for deploy

---
+++ public/react/precache-manifest.3dfd880821b56f91dacf6513ee2538fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6ffaf9eaa0325c038ad4a78d630ec52",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c49a439aeb24ae1639cc",
+    "url": "/static/css/main.458c5bca.chunk.css"
+  },
+  {
+    "revision": "46a31c6138ec587d5960",
+    "url": "/static/js/2.a0df2612.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a0df2612.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c49a439aeb24ae1639cc",
+    "url": "/static/js/main.7e8c54f7.chunk.js"
+  },
+  {
+    "revision": "2cfd8637903636794dbb",
+    "url": "/static/js/runtime-main.3f182f84.js"
+  }
+]);
